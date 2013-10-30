302eba12a26e122f7418ff105278325ed25aae07
bigshotrob22 <rrascon22@gmail.com>
2013-10-30 14:53:20 -0700

    Remove common_cdma.mk import
    Change-Id: If71446ecf4587867fa94a1fd40c5ec6863df582d

--- carbon.mk
+++ carbon.mk
@@ -1,9 +1,6 @@
 $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/carbon/config/common_cdma.mk)
-
-# Inherit some common CM stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys"
