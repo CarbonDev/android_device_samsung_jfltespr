18709165400188d1c9d43776e9696b4c2f5fe73d
Patrick Campanale <patrickappcreator@gmail.com>
2014-02-12 13:58:02 -0800

    Cleanup carbon.mk
    remove unneeded lines
    
    Change-Id: I3215c586ef1e6dba63d2f63b710091e5966a5485

--- carbon.mk
+++ carbon.mk
@@ -1,16 +1,9 @@
 $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 
-# Inherit some common CM stuff.
-#$(call inherit-product, vendor/carbon/config/cdma.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
+# Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys"
-
 
 PRODUCT_NAME := carbon_jfltespr
 PRODUCT_DEVICE := jfltespr
